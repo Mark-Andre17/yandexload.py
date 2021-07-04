2eabc2295c249c9968702771ef74ae4e8d8ea1ce
Mark-Andre17 <84619111+Mark-Andre17@users.noreply.github.com>
2021-07-05 01:52:15 +0300

    stack commit

--- yandexload.py
+++ yandexload.py
@@ -1,15 +1,44 @@
-import yadisk
+from pprint import pprint
+
+import requests
 
 
-def load_file():
-    y = yadisk.YaDisk(token=input('вставьте свой токен:'))
-    yadate = input('Название папки на яндекс диске:')
-    y.mkdir(f'/{yadate}')
-    disk = input('Какой диск:')
-    date = input('Какая папка:')
-    file = input('Файл с расширением:')
-    y.upload(f'{disk}://{date}/{file}', f'/{yadate}/{file}')
+class DiskUpload:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def get_files_list(self):
+        files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        headers = self.get_headers()
+        response = requests.get(files_url, headers=headers)
+        return response.json()
+
+    def get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload_to_disk(self, disk_file_path, filename):
+        href = self.get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
 
 
 
-load_file()
+
+
+if __name__ == '__main__':
+    ya = DiskUpload(token="")
+    ya.upload_to_disk("", "")
